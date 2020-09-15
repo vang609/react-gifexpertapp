475af1ad48e13ce34bdcba7c970e92cb1ffdcac7
“e-Vang” <enrique.vangz@gmail.com>
2020-09-15 00:14:12 -0400

    02 Adding docs folder

---
+++ docs/precache-manifest.94c7fa74188899697eba20c4673c1c56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1e7ab44b759b8f3b9da311452f34c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29fab75c0c838112db48",
+    "url": "/static/css/main.621a6f97.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29fab75c0c838112db48",
+    "url": "/static/js/main.2aa717f0.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
