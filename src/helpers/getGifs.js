2601a2edf31935178b2464ab20ba0fe3bf31cb68
“e-Vang” <enrique.vangz@gmail.com>
2020-09-15 09:44:37 -0400

    04 change api key

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -3,7 +3,7 @@
 
 export const getGifs = async( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=A8xMXqzieIHmtO3BjGLAtf1daSSDAv8K`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=QTURaJaZAEl09q57BAi0NvDBsEhoxmXc`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
